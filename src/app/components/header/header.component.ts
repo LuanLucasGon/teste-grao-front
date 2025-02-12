4481ae6a359f447863fe2ce8f83bd0dbf03c2ee1
LuanLucasGon <lucasluangon@gmail.com>
2025-02-12 16:24:19 -0300

    feat: cerating app header

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, FontAwesomeModule, FormsModule, RouterModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  faSearch = faSearch;
+  @Input() logoPath: string = 'assets/images/logo.png';
+  @Input() siteName: string = 'Quem tem boca';
+
+  searchQuery: string = '';
+
+  onSearch(): void {
+    console.log('Pesquisando por:', this.searchQuery);
+  }
+}
